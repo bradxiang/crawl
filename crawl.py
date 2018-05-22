6e62c2e689414463cba0a00244f4cf057e9a8fa6
bradxiang <xiangxyyu@126.com>
2018-05-22 16:13:18 +0800

    实现协程同步和异步执行

--- crawl.py
+++ crawl.py
@@ -1,6 +1,7 @@
-from lianjia import CrawlerLianjia
+from sample import AsyncSample
 from threading import Thread
 import asyncio
+import time
 
 
 def start_loop(loop):
@@ -9,18 +10,30 @@ def start_loop(loop):
 
 
 if __name__ == '__main__':
-    # 链接抓取
-    max_page = 2000  # 读取的页数
-    lianjia_loop = asyncio.new_event_loop()
-    crawler = CrawlerLianjia(max_page, max_tasks=66,
-                             _loop=lianjia_loop)  # 协程数量 采集url
-    t = Thread(target=start_loop, args=(lianjia_loop,), name="lianjia")
-    t.start()
-    asyncio.run_coroutine_threadsafe(crawler.run(), lianjia_loop)
 
-    # 本地线程执行
-    # max_page = 2
+    # # 本地线程执行
+    # max_page = 10000
     # loop = asyncio.get_event_loop()
-    # crawler = CrawlerLianjia(max_page, max_tasks=66)  # 协程数量 采集url
+    # crawler = AsyncSample(max_page, max_tasks=2)  # 协程数量 采集url
     # loop.run_until_complete(crawler.run())
-    # loop.close()
+    # print(100)
+
+
+    # 创建新线程异步操作，永久运行线程
+    new_loop = asyncio.new_event_loop()
+    t = Thread(target=start_loop, args=(new_loop,))
+    t.setDaemon(True)  # 设置子线程为守护线程
+    t.start()
+    try:
+        while True:
+            max_page = 2000  # 读取的页数
+            crawler = AsyncSample(max_page, max_tasks=20,
+                                  _loop=new_loop)  # 协程数量 采集url
+            future = asyncio.run_coroutine_threadsafe(crawler.run(), new_loop)
+            while (not future.done()):
+                time.sleep(0.1)
+    except KeyboardInterrupt as e:
+        print(e)
+        new_loop.stop()
+        print('结束运行')
+
